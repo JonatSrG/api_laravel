8562eed2cf7a105a3341b9b3537d53415a308ed4
JonatSrG <JonatSrG>
2021-02-25 11:43:10 -0600

    validacion de index contdd

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -12,11 +12,6 @@ class PostTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** 
-     * Los usuarios no autenticados no pueden acceder al api de post
-     * 
-     * @test
-    */
     public function unauthenticated_users_cannot_access_the_post_api()
     {
         //$this->withoutExceptionHandling();
@@ -27,102 +22,6 @@ class PostTest extends TestCase
         $this->json('PUT',     '/api/posts/1000')->assertStatus(401);
         $this->json('DELETE',  '/api/posts/1000')->assertStatus(401);
     }
-
-    /**
-     * Los usuarios autenticados pueden acceder al listado de posts
-     * 
-     * @test
-     */
-    public function can_see_paginated_post_list()
-    {
-        $this->withoutExceptionHandling();
-
-        $user = factory(User::class)->create();
-        factory(Post::class, 5)->create();
-
-        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts');
-
-        $response->assertJsonStructure([
-            'data' => [
-                '*' => ['id', 'title', 'created']
-            ],
-            'links' => ['first', 'last', 'prev', 'next'],
-        ])->assertStatus(200);
-    }
-
-    /**
-     * los usuarios ayenticados pueden crear un post
-     * 
-     * @test
-     */
-    public function a_user_can_create_a_post()
-    {
-        //$this->withoutExceptionHandling();
-
-        $user = factory(User::class)->create();
-
-        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
-            'title' => 'Post de prueba'
-        ]);
-
-        $response->assertJsonStructure(['id', 'title', 'created'])
-        ->assertJson(['tilte' => 'Post de prueba'])
-        ->assertStatus(201);
-
-        $this->assertDatabaseHas('posts', [
-            'title' => 'Post de prueba'
-        ]);
-    }
-
-    /**
-     * validadndo si crea un post con el titulo vacio
-     * 
-     * @test
-     */
-    public function validation_if_you_create_a_post_with_the_empty_title()
-    {
-        //$this->withoutExceptionHandling();
-
-        $user = factory(User::class)->create();
-
-        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
-            'title' => ''
-        ]);
-
-        $response->assertStatus(422)
-        //->assertExactValidationErrors('title')
-        ->assertExactJson([
-            'message' => 'The given data was invalid',
-            'errors' => [
-                'title' => ['The title field is required']
-            ]
-        ]);
-    }
-
-    /**
-     * Puede acceder y ver un post
-     * 
-     * @test
-     */
-    public function can_get_a_post()
-    {
-        //$this->withoutExceptionHandling();
-
-        $user = factory(User::class)->create();
-        $post = factory(Post::class)->create();
-
-        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts/$post->id');
-
-        $response->assertJsonStructure(['id', 'title', 'created'])
-        ->assertJson(['title' => $post->title])
-        ->assertStatus(200);
-    }
-
-    /**
-     * si el post no existe recibira un 404
-     * 
-     * @test
-     */
 
     public function test_store()
     {
@@ -193,6 +92,19 @@ class PostTest extends TestCase
         ->assertStatus(204);//sin contenido
 
         $this->assertDatabaseMissing('posts', ['id' => $post->id]);
+    }
+
+    public function test_index()
+    {
+        factory(Post::class)->create();
+
+        $response = $this->json('GET', '/api/posts');
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => ['id', 'title', 'created_at', 'update_at']
+            ]
+        ])->assertStatus(200);//estatus de ok
     }
 
 }
